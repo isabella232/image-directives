1ae91411d3172c23c2949f194f71fe0b9e3ee098
Russ Savage <russ@cask.co>
2017-08-04 15:06:44 -0700

    initial commit

---
+++ src/main/java/org/example/directives/ExtractEXIF.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright © 2017 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package org.example.directives;
+
+import co.cask.cdap.api.annotation.Description;
+import co.cask.cdap.api.annotation.Name;
+import co.cask.cdap.api.annotation.Plugin;
+import co.cask.wrangler.api.Arguments;
+import co.cask.wrangler.api.Directive;
+import co.cask.wrangler.api.DirectiveExecutionException;
+import co.cask.wrangler.api.DirectiveParseException;
+import co.cask.wrangler.api.ErrorRowException;
+import co.cask.wrangler.api.ExecutorContext;
+import co.cask.wrangler.api.Row;
+import co.cask.wrangler.api.parser.ColumnName;
+import co.cask.wrangler.api.parser.TokenType;
+import co.cask.wrangler.api.parser.UsageDefinition;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+/**
+ * This class <code>ExtractEXIF</code>implements a <code>Directive</code> interface
+ * for reversing the text specified by the value of the <code>column</code>.
+ */
+@Plugin(type = Directive.Type)
+@Name(ExtractEXIF.DIRECTIVE_NAME)
+@Description(ExtractEXIF.DIRECTIVE_DESC)
+public final class ExtractEXIF implements Directive {
+  public static final String DIRECTIVE_NAME = "exif";
+  public static final String DIRECTIVE_DESC = "Extracts EXIF data from binary image files.";
+
+  private String sourceColumn;
+
+  @Override
+  public UsageDefinition define() {
+    // Usage : exif :binary-image-column;
+    UsageDefinition.Builder builder = UsageDefinition.builder(DIRECTIVE_NAME);
+    builder.define("binaryImageColumn", TokenType.COLUMN_NAME);
+    return builder.build();
+  }
+
+  @Override
+  public void initialize(Arguments args)
+    throws DirectiveParseException {
+    sourceColumn = ((ColumnName) args.value("binaryImageColumn")).value();
+  }
+
+  @Override
+  public List<Row> execute(List<Row> rows, ExecutorContext context)
+    throws DirectiveExecutionException, ErrorRowException {
+    for (Row row : rows) {
+      int sourceColumnIndex = row.find(sourceColumn);
+      if (sourceColumnIndex != -1) {
+        try {
+          Object imageData = row.getValue(sourceColumnIndex);
+          if (imageData instanceof byte[]) {
+            Metadata metadata = ImageMetadataReader.readMetadata(new ByteArrayInputStream((byte[]) imageData));
+            for (Directory directory : metadata.getDirectories()) {
+              for (Tag tag : directory.getTags()) {
+                StringBuilder tagName = new StringBuilder();
+                tagName.append(directory.getName().replaceAll(" ", "_"))
+                  .append("_")
+                  .append(tag.getTagName().replaceAll(" ", "_"));
+                String description = tag.getDescription();
+                if (description == null) {
+                  description = directory.getString(tag.getTagType()) + " (unable to formulate description)";
+                }
+                row.addOrSet(tagName.toString(), description);
+              }
+            }
+          }
+        } catch (Exception e) {
+
+        }
+      }
+    }
+    return rows;
+  }
+
+  @Override
+  public void destroy() {
+    // no op
+  }
+}
---
+++ src/test/java/org/example/directives/ExtractEXIFTest.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright © 2017 Cask Data, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy of
+ *  the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations under
+ *  the License.
+ */
+
+package org.example.directives;
+
+import co.cask.wrangler.api.RecipePipeline;
+import co.cask.wrangler.api.Row;
+import co.cask.wrangler.test.TestingRig;
+import co.cask.wrangler.test.api.TestRecipe;
+import co.cask.wrangler.test.api.TestRows;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Tests {@link ExtractEXIF}
+ */
+public class ExtractEXIFTest {
+
+  @Ignore
+  @Test
+  public void testBasicReverse() throws Exception {
+    TestRecipe recipe = new TestRecipe();
+    recipe.add("exif :body;");
+    String fileName = "10x12x16bit-CMYK.psd";
+    ClassLoader classLoader = getClass().getClassLoader();
+    URL imageFile = classLoader.getResource(fileName);
+    Path path = Paths.get(imageFile.getPath());
+
+    TestRows rows = new TestRows();
+    rows.add(new Row("body", Files.readAllBytes(path)));
+
+    RecipePipeline pipeline = TestingRig.pipeline(ExtractEXIF.class, recipe);
+    List<Row> actual = pipeline.execute(rows.toList());
+
+    Assert.assertEquals(1, actual.size());
+    Assert.assertEquals(73, actual.get(0).getFields().size());
+  }
+}
